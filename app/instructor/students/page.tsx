a16102f30b69ba8ed13a373aa2f40c4a076919b0
Edison Florian <florianedison22@gmail.com>
2026-03-24 00:58:27 -0400

    Participants Session Feedback implemented

---
+++ app/instructor/students/page.tsx
@@ -0,0 +1,8 @@
+export default function StudentsPage() {
+	return (
+		<div className="p-4">
+			<h1 className="text-2xl font-semibold">Students</h1>
+			<p>Instructor students list will go here.</p>
+		</div>
+	)
+}
